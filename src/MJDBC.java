790aad89634cbe58fe75da7b2fe49731a2c090bf
Max Bolingbroke <batterseapower@hotmail.com>
2015-11-13 22:17:47 +0000

    Support construction with a DataSource as well

--- src/MJDBC.java
+++ src/MJDBC.java
@@ -1,30 +1,61 @@
+
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class MJDBC {
+    private interface ConnectionObtainer {
+        <T> T with(ConnectionUser<T> user) throws SQLException;
+
+        static ConnectionObtainer fromDataSource(DataSource dataSource) {
+            return new ConnectionObtainer() {
+                @Override
+                public <T> T with(ConnectionUser<T> user) throws SQLException {
+                    try (final Connection c = dataSource.getConnection()) {
+                        return user.consume(c);
+                    }
+                }
+            };
+        }
+
+        static ConnectionObtainer fromConnection(Connection connection) {
+            return new ConnectionObtainer() {
+                @Override
+                public <T> T with(ConnectionUser<T> user) throws SQLException {
+                    return user.consume(connection);
+                }
+            };
+        }
+    }
+
+    private interface ConnectionUser<T> {
+        T consume(Connection c) throws SQLException;
+    }
+
     private final Context context;
-    private final Connection connection;
+    private final ConnectionObtainer connectionObtainer;
     private final boolean prepared;
 
-    // FIXME: support DataSource
-    // FIXME: named params? Maybe.
     // FIXME: transactions
     // FIXME: retry deadlocks
     public MJDBC(Context context, Connection connection) {
-        this(context, connection, true);
+        this(context, ConnectionObtainer.fromConnection(connection), true);
+    }
+    public MJDBC(Context context, DataSource dataSource) {
+        this(context, ConnectionObtainer.fromDataSource(dataSource), true);
     }
 
-    private MJDBC(Context context, Connection connection,
+    private MJDBC(Context context, ConnectionObtainer connectionObtainer,
                   boolean prepared) {
         this.context = context;
-        this.connection = connection;
+        this.connectionObtainer = connectionObtainer;
         this.prepared = prepared;
     }
 
     public MJDBC withPrepared(boolean prepared) {
-        return new MJDBC(context, connection, prepared);
+        return new MJDBC(context, connectionObtainer, prepared);
     }
 
     public void execute(SQL sql) throws SQLException {
@@ -38,45 +69,49 @@ public class MJDBC {
         if (prepared) {
             final BatchPreparedSQLBuilder builder = new BatchPreparedSQLBuilder(context.writers);
             builder.visitSQL(sql);
-            try (final PreparedStatement ps = builder.build(connection)) {
-                try {
-                    return ps.executeLargeBatch();
-                } catch (UnsupportedOperationException _) {
-                    final int[] ints = ps.executeBatch();
-                    final long[] longs = new long[ints.length];
-                    for (int i = 0; i < ints.length; i++) {
-                        longs[i] = ints[i];
+            return connectionObtainer.with(c -> {
+                try (final PreparedStatement ps = builder.build(c)) {
+                    try {
+                        return ps.executeLargeBatch();
+                    } catch (UnsupportedOperationException _) {
+                        final int[] ints = ps.executeBatch();
+                        final long[] longs = new long[ints.length];
+                        for (int i = 0; i < ints.length; i++) {
+                            longs[i] = ints[i];
+                        }
+                        return longs;
                     }
-                    return longs;
                 }
-            }
+            });
         } else {
             final BatchUnpreparedSQLBuilder builder = new BatchUnpreparedSQLBuilder(context.writers);
             builder.visitSQL(sql);
-            try (final Statement s = connection.createStatement()) {
-                final Map.Entry<Integer, Iterator<String>> e = builder.buildIterator();
-                final Iterator<String> it = e.getValue();
-                final long[] result = new long[e.getKey()];
+            return connectionObtainer.with(c -> {
+                try (final Statement s = c.createStatement()) {
+                    final Map.Entry<Integer, Iterator<String>> e = builder.buildIterator();
+                    final Iterator<String> it = e.getValue();
+                    final long[] result = new long[e.getKey()];
 
-                boolean supportsLargeUpdate = true;
-                int i = 0;
-                while (it.hasNext()) {
-                    final String x = it.next();
-                    if (!supportsLargeUpdate) {
-                        result[i] = s.executeUpdate(x);
-                    } else {
-                        try {
-                            result[i] = s.executeLargeUpdate(x);
-                        } catch (UnsupportedOperationException _) {
-                            supportsLargeUpdate = false;
+                    boolean supportsLargeUpdate = true;
+                    int i = 0;
+                    while (it.hasNext()) {
+                        final String x = it.next();
+                        if (!supportsLargeUpdate) {
                             result[i] = s.executeUpdate(x);
+                        } else {
+                            try {
+                                result[i] = s.executeLargeUpdate(x);
+                            } catch (UnsupportedOperationException _) {
+                                supportsLargeUpdate = false;
+                                result[i] = s.executeUpdate(x);
+                            }
                         }
+                        i++;
                     }
-                    i++;
-                }
 
-                return result;
-            }
+                    return result;
+                }
+            });
         }
     }
 
@@ -110,15 +145,19 @@ public class MJDBC {
         if (prepared) {
             final PreparedSQLBuilder builder = new PreparedSQLBuilder(context.writers);
             builder.visitSQL(sql);
-            try (final PreparedStatement ps = builder.build(connection)) {
-                return batchRead.get(context.readers, new PreparedStatementlike(ps));
-            }
+            return connectionObtainer.with(c -> {
+                try (final PreparedStatement ps = builder.build(c)) {
+                    return batchRead.get(context.readers, new PreparedStatementlike(ps));
+                }
+            });
         } else {
             final UnpreparedSQLBuilder builder = new UnpreparedSQLBuilder(context.writers);
             builder.visitSQL(sql);
-            try (final Statement s = connection.createStatement()) {
-                return batchRead.get(context.readers, new UnpreparedStamentlike(s, builder.build()));
-            }
+            return connectionObtainer.with(c -> {
+                try (final Statement s = c.createStatement()) {
+                    return batchRead.get(context.readers, new UnpreparedStamentlike(s, builder.build()));
+                }
+            });
         }
     }
 }
