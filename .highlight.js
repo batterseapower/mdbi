d8b8e988d8618de7fd9bc3bffd10ad3f39e96e8e
Max Bolingbroke <batterseapower@hotmail.com>
2015-11-20 23:29:37 +0000

    highlight.js

---
+++ .highlight.js
@@ -0,0 +1,4 @@
+
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.9.1/styles/default.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.9.1/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
