15dba4838a849b4c7ddc2ed4f94fe56b986684b9
Max Bolingbroke <batterseapower@hotmail.com>
2015-11-30 18:43:39 +0000

    JavaDoc fixes

--- src/main/java/uk/co/omegaprime/mdbi/BatchRead.java
+++ src/main/java/uk/co/omegaprime/mdbi/BatchRead.java
@@ -3,19 +3,21 @@ package uk.co.omegaprime.mdbi;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/** A slightly simplified version of {@link StatementlikeBatchRead} that consumes a {@code ResultSet}. */
+/**
+ * A slightly simplified version of {@link StatementlikeBatchRead} that consumes a {@code ResultSet}.
+ * <p>
+ * Useful instances of this can be obtained from {@link BatchReads}.
+ */
 public interface BatchRead<T> {
     /**
      * Consume one or more row from the {@code ResultSet}, returning a corresponding Java object.
-     *
+     * <p>
+     * When it arrives, the {@code ResultSet} will be on the row before the first one that you can consume -- i.e.
+     * it will not be on a valid row. Consequently you need to call {@link ResultSet#next()} before you access any column.
+     * <p>
      * If you cease consuming the {@code ResultSet} early, you should leave the {@code ResultSet}
-     * on the first row that you have decided not to consume. An exception to this is if you only want
-     * to consume a single row from the input, in which case you are permitted to leave the {@code ResultSet}
-     * on the row in question. This is a performance optimization intended to avoid the overhead of {@link ResultSet#next()} in
-     * the common case where you only want the first row from a {@code ResultSet}.
-     *
-     * As a result of this optimization, there is no way to signal that you have consumed zero rows
-     * from the {@code ResultSet}.
+     * on the last row that you have decided to consume. This means that there is no difference between
+     * leaving the {@code ResultSet} on the final row and leaving it after the end of the whole {@code ResultSet}.
      */
     T get(Read.Context ctxt, ResultSet rs) throws SQLException;
 }
